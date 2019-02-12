d0c79413a83efb8eb53577f7041983edb66832f6
ivandrew05 <46248495+ivandrew05@users.noreply.github.com>
2019-02-12 21:12:33 +0800

    Add files via upload

--- musicplayer.py
+++ musicplayer.py
@@ -6,6 +6,7 @@ from tkinter import *
 from tkinter import ttk
 from tkinter import messagebox
 from tkinter import filedialog
+from random import randrange
 
 from pygame import mixer
 from mutagen.mp3 import MP3
@@ -165,34 +166,35 @@ def start_count(t):
 #创建几个global variables
 playing=False
 pause=False
-selectedsong_index=0
+selected_song_index=0
 loop=False
 mute=False
-running =False
+running=False
+shuffle=False
 
 #定义function播放暂停音乐
-def playpause_music():
+def play_pause_music():
     global playing
-    global selectedsong_index
+    global selected_song_index
     if playing: #if playing=True
         global pause
         if pause: #if pause=True
             mixer.music.unpause()
             playpausebutton.configure(image=pausephoto)
-            statusbar["text"]="正在播放-"+os.path.basename(playlist[selectedsong_index])
+            statusbar["text"]="正在播放-"+os.path.basename(playlist[selected_song_index])
             pause=False
         else:  #if pause=False
             mixer.music.pause()
             playpausebutton.configure(image=playphoto)
-            statusbar["text"]="已暂停播放-"+os.path.basename(playlist[selectedsong_index])
+            statusbar["text"]="已暂停播放-"+os.path.basename(playlist[selected_song_index])
             pause=True
     else:  #if playing=False
         try:
             stop_music()
             time.sleep(0.1)
             selected_song=playlistbox.curselection()
-            selectedsong_index=int(selected_song[0])
-            play_it=playlist[selectedsong_index]
+            selected_song_index=int(selected_song[0])
+            play_it=playlist[selected_song_index]
             mixer.music.load(play_it)
             mixer.music.play()
             statusbar["text"]="正在播放-"+os.path.basename(play_it)
@@ -204,55 +206,55 @@ def playpause_music():
 
 #定义function播放下一首
 def play_next():
-    global selectedsong_index
+    global selected_song_index
     global playing
     global pause
-    stop_music()
+    mixer.music.stop()
     time.sleep(0.1)
     a=len(playlist)-1
-    if selectedsong_index<a:
-        selectedsong_index=selectedsong_index+1
-        mixer.music.load(playlist[selectedsong_index])
+    if selected_song_index<a:
+        selected_song_index=selected_song_index+1
+        mixer.music.load(playlist[selected_song_index])
         mixer.music.play()
-        statusbar["text"]="正在播放-"+os.path.basename(playlist[selectedsong_index])
-        show_details(playlist[selectedsong_index])
+        statusbar["text"]="正在播放-"+os.path.basename(playlist[selected_song_index])
+        show_details(playlist[selected_song_index])
     else:
         mixer.music.load(playlist[0])
         mixer.music.play()
         statusbar["text"]="正在播放-"+os.path.basename(playlist[0])
         show_details(playlist[0])
-        selectedsong_index=0
+        selected_song_index=0
     playpausebutton.configure(image=pausephoto)
     playlistbox.selection_clear(0, END)    #清空playlistbox的选择
-    playlistbox.selection_set(selectedsong_index)    #设置playlistbox的选择
-    playlistbox.see(selectedsong_index)    #保证选择的item在playlistbox中可见
+    playlistbox.selection_set(selected_song_index)    #设置playlistbox的选择
+    playlistbox.see(selected_song_index)    #保证选择的item在playlistbox中可见
     playing=True
     pause=False
 
 #定义function播放上一首
 def play_previous():
-    global selectedsong_index
+    global selected_song_index
     global playing
     global pause
-    stop_music()
+    mixer.music.stop()
     time.sleep(0.1)
     a=len(playlist)-1
-    if selectedsong_index==0:
+    if selected_song_index==0:
         mixer.music.load(playlist[-1])
         mixer.music.play()
         statusbar["text"]="正在播放-"+os.path.basename(playlist[-1])
         show_details(playlist[-1])
-        selectedsong_index=a
+        selected_song_index=a
     else:
-        selectedsong_index=selectedsong_index-1
-        mixer.music.load(playlist[selectedsong_index])
+        selected_song_index=selected_song_index-1
+        mixer.music.load(playlist[selected_song_index])
         mixer.music.play()
-        statusbar["text"]="正在播放-"+os.path.basename(playlist[selectedsong_index])
-        show_details(playlist[selectedsong_index])
+        statusbar["text"]="正在播放-"+os.path.basename(playlist[selected_song_index])
+        show_details(playlist[selected_song_index])
     playpausebutton.configure(image=pausephoto)
     playlistbox.selection_clear(0, END)    #清空playlistbox的选择
-    playlistbox.selection_set(selectedsong_index)    #设置playlistbox的选择
-    playlistbox.see(selectedsong_index)    #保证选择的item在playlistbox中可见
+    playlistbox.selection_set(selected_song_index)    #设置playlistbox的选择
+    playlistbox.see(selected_song_index)    #保证选择的item在playlistbox中可见
     playing=True
     pause=False
     
@@ -270,14 +272,12 @@ def stop_music():
 def loop_playlist():
     global running
     global playing
-    global selectedsong_index
     while running:
         if mixer.music.get_busy() or playing==False:
             time.sleep(1.0)
         else:
             play_next()
-            print(selectedsong_index)
-                     
+            
 #定义function循环播放音乐
 def loop_music():
     global loop
@@ -296,20 +296,70 @@ def loop_music():
         print(a)
         #threading.enumerate() returns a list of all Thread objects currently alive,
         #It excludes terminated threads and threads that have not yet been started.
-                          
+
+#定义function随机播放
+def random_play():
+    global playing
+    time.sleep(0.1)
+    selected_song=playlistbox.curselection()
+    selected_song_index=int(selected_song[0])
+    random_song_index=randrange(len(playlist))    #随机从playlist中选出一个item的index
+    #while loop确保随机选中的index不是正在播放歌曲的index
+    while random_song_index==selected_song_index:
+        random_song_index=randrange(len(playlist))
+    random_song=playlist[random_song_index]
+    mixer.music.load(random_song)
+    mixer.music.play()
+    playlistbox.selection_clear(0, END)    #清空playlistbox的选择
+    playlistbox.selection_set(random_song_index)    #设置playlistbox的选择
+    playlistbox.see(random_song_index)    #保证选择的item在playlistbox中可见
+    statusbar["text"]="正在播放-"+os.path.basename(random_song)
+    show_details(random_song)
+    playpausebutton.configure(image=pausephoto)
+    playing=True
+
+#定义function随机播放列表
+def shuffle_playlist():
+    global running
+    global playing
+    while running:
+        if mixer.music.get_busy() or playing==False:
+            time.sleep(1.0)
+        else:
+            random_play()
+            
+#定义function随机播放音乐
+def shuffle_music():
+    global shuffle
+    global running
+    if shuffle==False:
+        running=True
+        shufflebutton.configure(image=shuffleonphoto)
+        t3=threading.Thread(target=shuffle_playlist)
+        t3.start()
+        shuffle=True
+    else:    # if shuffle==True
+        running=False
+        shufflebutton.configure(image=shuffleoffphoto)
+        shuffle=False
+        a=threading.enumerate()
+        print(a)
+        #threading.enumerate() returns a list of all Thread objects currently alive,
+        #It excludes terminated threads and threads that have not yet been started.
+        
 #定义function双击播放
 def double_click(event):
-    global selectedsong_index
+    global selected_song_index
     global playing
     global pause
     stop_music()
     time.sleep(0.1)
     selected_song=playlistbox.curselection()
-    selectedsong_index=int(selected_song[0])
-    mixer.music.load(playlist[selectedsong_index])
+    selected_song_index=int(selected_song[0])
+    mixer.music.load(playlist[selected_song_index])
     mixer.music.play()
-    statusbar["text"]="正在播放-"+os.path.basename(playlist[selectedsong_index])
-    show_details(playlist[selectedsong_index])
+    statusbar["text"]="正在播放-"+os.path.basename(playlist[selected_song_index])
+    show_details(playlist[selected_song_index])
     playpausebutton.configure(image=pausephoto)
     playing=True
     pause=False
@@ -355,7 +405,7 @@ delbutton.pack(side=LEFT, padx=3)
 #创建播放/暂停按钮
 playphoto=PhotoImage(file="images/play.png")
 pausephoto=PhotoImage(file="images/pause.png")
-playpausebutton=ttk.Button(middleframe, image=playphoto, command= playpause_music)
+playpausebutton=ttk.Button(middleframe, image=playphoto, command= play_pause_music)
 playpausebutton.grid(row=0, column=1, padx=10)
 
 #创建上一首按钮
@@ -377,7 +427,13 @@ stopbutton.grid(row=0, column=3, padx=10)
 loopoffphoto=PhotoImage(file="images/loopoff.png")
 looponphoto=PhotoImage(file="images/loopon.png")
 loopbutton=ttk.Button(bottomframe, image=loopoffphoto, command=loop_music)
-loopbutton.grid(row=0, column=0, padx=10)
+loopbutton.grid(row=0, column=0)
+
+#创建随机播放按钮
+shuffleoffphoto=PhotoImage(file="images/shuffleoff.png")
+shuffleonphoto=PhotoImage(file="images/shuffleon.png")
+shufflebutton=ttk.Button(bottomframe, image=shuffleoffphoto, command=shuffle_music)
+shufflebutton.grid(row=0, column=1, padx=6)
 
 #创建消音按钮和4个音量图标
 volmutephoto=PhotoImage(file="images/volmute.png")
@@ -385,14 +441,14 @@ volminphoto=PhotoImage(file="images/volmin.png")
 volmidphoto=PhotoImage(file="images/volmid.png")
 volmaxphoto=PhotoImage(file="images/volmax.png")
 volumebutton=ttk.Button(bottomframe, image=volmidphoto, command=mute_music)
-volumebutton.grid(row=0, column=1)
+volumebutton.grid(row=0, column=2)
 
 #创建音量条并设置默认音量
 scale=ttk.Scale(bottomframe, from_=0, to=100, orient=HORIZONTAL, command=set_vol)
 scale.config(length=130)    #设置scale的长度
 scale.set(70)
 mixer.music.set_volume(0.7)
-scale.grid(row=0, column=2)
+scale.grid(row=0, column=3)
 
 #绑定双击playlistbox时，执行double_click
 playlistbox.bind('<Double-1>', double_click)
